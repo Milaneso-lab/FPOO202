472bf907705ca030320283667c4a9a9c07a73dc5
Antonio Madriz <123045553@upq.edu.mx>
2025-01-30 14:45:39 -0600

    Practica 4 - Problema 2

--- Condicionales/src/Problema2.java
+++ Condicionales/src/Problema2.java
@@ -4,6 +4,13 @@ public class Problema2 {
     public static void main(String[] args){
         Scanner scn = new Scanner(System.in);
 
-        
+        System.out.println("Ingresa un numero entero: ");
+        int numero = scn.nextInt();
+
+        if (numero%2==0){
+            System.out.println("El numero es par");
+        }else {
+            System.out.println("El numero es impar");
+        }
     }
 }
