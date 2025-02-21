1e07a751408f8d671f436adc1c1e6f63b1d54ae0
Antonio Madriz <123045553@upq.edu.mx>
2025-02-21 09:21:05 -0600

    Practica No 9 - Terminada

--- Cuenta_Popular/src/Cuenta.java
+++ Cuenta_Popular/src/Cuenta.java
@@ -11,27 +11,27 @@ public class Cuenta {
         JOptionPane.showMessageDialog(null, "El saldo es de: " + saldo);
     }
 
-    public void Ingresar () {
-        monto = Float.parseFloat(JOptionPane.showInputDialog("Ingrese el monto a ingresar:"));
+    public void Ingresar (float monto) {
+        monto = (int) Float.parseFloat(JOptionPane.showInputDialog("Ingrese el monto a ingresar:"));
         float total = saldo + monto;
         JOptionPane.showMessageDialog(null, "El saldo actual de la cuenta es: " + total);
     }
 
-    public void Retirar () {
-        monto = Float.parseFloat(JOptionPane.showInputDialog("Ingrese el monto a retirar:"));
+    public void Retirar (float Monto) {
+        Monto = Float.parseFloat(JOptionPane.showInputDialog("Ingrese el monto a retirar:"));
         if (saldo > 0) {
-            float SaldoNuevo = saldo - monto;
+            float SaldoNuevo = saldo - Monto;
             JOptionPane.showMessageDialog(null, "El saldo actual es de: " + SaldoNuevo);
         } else {
             JOptionPane.showMessageDialog(null, "El saldo es insuficiente para el retiro");
         }
     }
 
-    public void Depositar () {
+    public void Depositar (Cuenta miCuenta, float montoDeposito) {
         Float.parseFloat(JOptionPane.showInputDialog("Ingrese el numero de cuenta destinatario:"));
-        monto = Float.parseFloat(JOptionPane.showInputDialog("Ingrese el monto a depositar:"));
+        montoDeposito = Float.parseFloat(JOptionPane.showInputDialog("Ingrese el monto a depositar:"));
         if (saldo > 0) {
-            float total = saldo - monto;
+            float total = saldo - montoDeposito;
             JOptionPane.showMessageDialog(null, "El deposito fue exitoso");
             JOptionPane.showMessageDialog(null, "El saldo actual es de: " + total);
         } else {
--- Cuenta_Popular/src/Main.java
+++ Cuenta_Popular/src/Main.java
@@ -6,10 +6,59 @@ public class Main {
         // Creacion de una instancia
         Cuenta Milaneso = new Cuenta();
 
-        Milaneso.No_cuenta = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de cuenta: "));
-        JOptionPane.showMessageDialog(null, "Bienvenido");
+        Milaneso.No_cuenta = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de cuenta:"));
+        Milaneso.titular = JOptionPane.showInputDialog("Ingrese el titular de la cuenta");
+        Milaneso.edad = String.valueOf((int) Double.parseDouble(JOptionPane.showInputDialog("Ingerse la edad del titular")));
+        Milaneso.saldo = (float) Double.parseDouble(JOptionPane.showInputDialog("Ingrese el saldo inicial"));
 
-        Milaneso.Consultar();
+        boolean salir = false;
+        while (!salir) {
+            String[] opciones = {"Consultar Saldo", "Ingresar Efectivo", "Retirar Efectivo", "Depositar a otra cuenta", "Salir"};
+            int opcion = JOptionPane.showOptionDialog(null, "Seleccione una opción:", "Administración de la cuenta", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opciones, opciones[0]);
 
+            switch (opcion) {
+                case 0:
+                    Milaneso.Consultar();
+                    break;
+
+                case 1:
+                    float monto = (float) Double.parseDouble(JOptionPane.showInputDialog("Ingrese la cantidad a ingresar: "));
+                    if(monto > 0 ) {
+                        Milaneso.Ingresar(monto);
+                    }else {
+                        JOptionPane.showMessageDialog(null, "La cantidad de ser positiva");
+                    }
+
+                    break;
+
+                case 2:
+                    float Monto = (float) Double.parseDouble(JOptionPane.showInputDialog("Ingrese la cantidad a retirar: "));
+                    if(Monto > 0) {
+                        Milaneso.Retirar(Monto);
+                    }else {
+                        JOptionPane.showMessageDialog(null,"La cantidad debe de ser positiva");
+                    }
+                    break;
+
+                case 3:
+                    Cuenta MiCuenta = new Cuenta();
+                    MiCuenta.No_cuenta = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cuenta a depositar"));
+                    MiCuenta.titular = JOptionPane.showInputDialog("Ingrese el titular de la cuenta");
+                    MiCuenta.edad = String.valueOf(Integer.parseInt((JOptionPane.showInputDialog("Ingrese la edad del titular de la cuenta: "))));
+                    MiCuenta.saldo = (float) Double.parseDouble(JOptionPane.showInputDialog("Ingrese el saldo inicial de la otra cuenta: "));
+                    float montoDeposito = (float) Double.parseDouble(JOptionPane.showInputDialog("Ingrese la cantidad a depositar: "));
+
+                    Milaneso.Depositar(MiCuenta, montoDeposito);
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opcion no válida.");
+
+                case 4 :
+                    salir = true;
+                    JOptionPane.showMessageDialog(null, "Gracias por su preferencia");
+            }
+
+        }
     }
 }
+
