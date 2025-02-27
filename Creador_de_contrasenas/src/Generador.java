59c1e8664fc3d6d38bb553d517a6bfe68e0939f1
Antonio Madriz <123045553@upq.edu.mx>
2025-02-27 15:19:51 -0600

    Practica 10 - Con Constructores y encapsulamiento

--- Creador_de_contrasenas/src/Generador.java
+++ Creador_de_contrasenas/src/Generador.java
@@ -3,46 +3,64 @@ import java.security.SecureRandom;
 import java.util.Random;
 
 public class Generador {
-
-    public String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
-    public String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    public String NUMBERS = "0123456789";
-    public String SPECIALS = "!@#$%^&*()-_=+<>?";
-    public int longitud;
-    public String Password;
-
+    private static final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+    private static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String NUMBERS = "0123456789";
+    private static final String SPECIALS = "!@#$%^&*()-_=+<>?";
+    private int longitud;
+    private String password;
     private final SecureRandom random = new SecureRandom();
 
-    public void ingresarLongitud (int longitud){
-        longitud = (int) Float.parseFloat(JOptionPane.showInputDialog("Ingresa la longitud de la contraseña:"));
-
+    // CONSTRUCTOR DEL PROGRAMA
+    public Generador(int longitud) {
+        this.longitud = longitud;
+        this.password = "";
     }
-        public String generatePassword(int length, boolean useUpper, boolean useSpecials) {
-            String charPool = LOWERCASE + NUMBERS;
-            if (useUpper) charPool += UPPERCASE;
-            if (useSpecials) charPool += SPECIALS;
 
-            StringBuilder password = new StringBuilder();
-            for (int i = 0; i < length; i++) {
-                password.append(charPool.charAt(random.nextInt(charPool.length())));
-            }
-            return password.toString();
+    // ZONA DE GETS Y SETS
+    public int getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(int longitud) {
+        this.longitud = longitud;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void ingresarLongitud() {
+        this.longitud = Integer.parseInt(JOptionPane.showInputDialog("Ingresa la longitud de la contraseña:"));
+    }
+
+    public String generatePassword(boolean useUpper, boolean useSpecials) {
+        String charPool = LOWERCASE + NUMBERS;
+        if (useUpper) charPool += UPPERCASE;
+        if (useSpecials) charPool += SPECIALS;
+
+        StringBuilder passwordBuilder = new StringBuilder();
+        for (int i = 0; i < longitud; i++) {
+            passwordBuilder.append(charPool.charAt(random.nextInt(charPool.length())));
         }
+        this.password = passwordBuilder.toString();
+        return this.password;
+    }
 
-        public String checkStrength(String password) {
-            int score = 0;
-            if (password.length() >= 8) score++;
-            if (password.matches(".*[A-Z].*")) score++;
-            if (password.matches(".*[0-9].*")) score++;
-            if (password.matches(".*[!@#$%^&*()-_=+<>?].*")) score++;
+    public String checkStrength() {
+        int score = 0;
+        if (password.length() >= 8) score++;
+        if (password.matches(".*[A-Z].*")) score++;
+        if (password.matches(".*[0-9].*")) score++;
+        if (password.matches(".*[!@#$%^&*()-_=+<>?].*")) score++;
 
-            switch (score) {
-                case 4: return "Fuerte";
-                case 3: return "Media";
-                case 2: return "Débil";
-                default: return "Muy débil";
-            }
+        switch (score) {
+            case 4: return "Fuerte";
+            case 3: return "Media";
+            case 2: return "Débil";
+            default: return "Muy débil";
         }
+    }
 
 }// Fin de la clase
 
--- Creador_de_contrasenas/src/Main.java
+++ Creador_de_contrasenas/src/Main.java
@@ -3,18 +3,18 @@ import javax.swing.JOptionPane;
 public class Main {
     public static void main(String[] args) {
 
-        Generador nueva = new Generador();
+            String inputLength = JOptionPane.showInputDialog("Ingrese la longitud del password (8 por defecto):");
+            int length = (inputLength != null && !inputLength.isEmpty()) ? Integer.parseInt(inputLength) : 8;
 
-        String inputLength = JOptionPane.showInputDialog("Ingrese la longitud del password (8 por defecto):");
-        int length = (inputLength != null && !inputLength.isEmpty()) ? Integer.parseInt(inputLength) : 8;
+            Generador nueva = new Generador(length);
 
-        int useUpper = JOptionPane.showConfirmDialog(null, "¿Incluir mayúsculas?", "Opciones", JOptionPane.YES_NO_OPTION);
-        int useSpecials = JOptionPane.showConfirmDialog(null, "¿Incluir caracteres especiales?", "Opciones", JOptionPane.YES_NO_OPTION);
+            int useUpper = JOptionPane.showConfirmDialog(null, "¿Incluir mayúsculas?", "Opciones", JOptionPane.YES_NO_OPTION);
+            int useSpecials = JOptionPane.showConfirmDialog(null, "¿Incluir caracteres especiales?", "Opciones", JOptionPane.YES_NO_OPTION);
 
-        String password = nueva.generatePassword(length, useUpper == JOptionPane.YES_OPTION, useSpecials == JOptionPane.YES_OPTION);
-        String strength = nueva.checkStrength(password);
+            String password = nueva.generatePassword(useUpper == JOptionPane.YES_OPTION, useSpecials == JOptionPane.YES_OPTION);
+            String strength = nueva.checkStrength();
 
-        JOptionPane.showMessageDialog(null, "Password generado: " + password + "\nFortaleza: " + strength);
+            JOptionPane.showMessageDialog(null, "Password generado: " + password + "\nFortaleza: " + strength);
 
     }
 }
