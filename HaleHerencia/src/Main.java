99b22e92930790b853b4cd958a02f59324bd8201
Antonio Madriz <123045553@upq.edu.mx>
2025-03-06 14:32:08 -0600

    Practica 14 - TERMINADA

--- HaleHerencia/src/Main.java
+++ HaleHerencia/src/Main.java
@@ -1,7 +1,31 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        // CREACION DE UN OBJETO
+        SpartanII JefeMaestro = new SpartanII("Jhon",98,"Rifle de asalto", 0);
+
+        // USO DE METODOS
+        JefeMaestro.mostrarInfo();
+        JefeMaestro.usarManejoAvanzado();
+        JefeMaestro.atacar("Covenant");
+        JefeMaestro.recibirDano(50);
+        JefeMaestro.recargarEscudo();
+
+        System.out.println("------------------------");
+
+        // CREACION DE UN SEGUNDO OBJETO
+        SpartanIII Noble6 = new SpartanIII("Antonio", 75,"Espada de energia",0);
+        SpartanIV Locke = new SpartanIV("Milaneso",100,"Pistola", 25);
+
+        // USO DE METODOS
+        Noble6.mostrarInfo();
+        Noble6.camuflajeActivo();
+        Noble6.recibirDano(10);
+
+        System.out.println("------------------");
+        Locke.mostrarInfo();
+        Locke.usarPropulsores();
+        Locke.atacar("Halo");
 
     }
 }
